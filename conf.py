3507546e2044a5a5ddfe658feb98f3bd63ae07ec
surekha19 <63954693+surekha19@users.noreply.github.com>
2020-08-28 17:47:12 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'Front End Development - Apssdc'
+copyright = '2020, Bhagi'
+author = 'Bhagi'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
